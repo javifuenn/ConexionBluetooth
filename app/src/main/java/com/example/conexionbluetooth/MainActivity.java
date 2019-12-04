ca3176dcc6bf0e98c15453257f44a0630329ff91
javifuenn <javifuenn@gmail.com>
2019-12-04 10:08:47 +0100

    Primera versión del código, funcionalidades básicas desarrolladas.

--- app/src/main/java/com/example/conexionbluetooth/MainActivity.java
+++ app/src/main/java/com/example/conexionbluetooth/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.conexionbluetooth;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -55,31 +60,75 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (!mBlueAdapter.isEnabled()) {
                     showToast("Activando Bluetooth...");
+                    //intent on Bluetooth
+                    Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE;
+                    startActivityForResult(intent, REQUEST_ENABLE_BT);
+                } else {
+                    showToast("El Bluetooth ya está activado");
                 }
             }
         });
-        // Cuando se pulsa el boton DESCUBRIR
+        // Cuando se pulsa el boton DETECTABLE
         mBotDescubrir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if (!mBlueAdapter.isEnabled()) {
+                    showToast("Dispositivo detectable...");
+                    Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                    startActivityForResult(intent, REQUEST_DISCOVER_BT);
+                }
             }
         });
         // Cuando se pulsa el boton OFF
         mBotOff.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if (!mBlueAdapter.isEnabled()) {
+                    mBlueAdapter.disable();
+                    showToast("Desactivando el Bluetooth...");
+                    mBluetIv.setImageResource(R.drawable.ic_action_off);
+                } else {
+                    showToast("El Bluetooth ya está desactivado");
+                }
             }
         });
         // Cuando se pulsa el boton DISPOSITIVOS EMPAREJADOS
         mBotEmp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if (!mBlueAdapter.isEnabled()) {
+                    mEmparejadoTv.setText("Dispositivos emparejados");
+                    showToast("Desactivando el Bluetooth...");
+                    Set<BluetoothDevice> devices = mBlueAdapter.getBondedDevices();
+                    for(BluetoothDevice device : devices) {
+                        mEmparejadoTv.append("\nDevice: " + device.getName() + "," + device);
+                    }
+                }
+                else {
+                    //El bluetooth está desactivado, así que no se puede mostrar la información requerida
+                    showToast("Turn on Bluetooth to get paired devices");
+                }
             }
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode) {
+            case REQUEST_ENABLE_BT:
+                if (resultCode == RESULT_OK) {
+                    //El Bluetooth está activado
+                    mBluetIv.setImageResource(R.drawable.ic_action_on);
+                    showToast("Bluetooth activado");
+                } else {
+                    // El usuario no activa el bluetooth
+                    showToast("No está activado");
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
     // Toast message (Mensaje que aparece y desaparece abajo en negro)
     private void showToast(String msg) {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
