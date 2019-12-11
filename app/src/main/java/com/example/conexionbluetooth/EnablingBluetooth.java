c0eff38c2a0107a8e18a144ae738f22652794b7d
javifuenn <javifuenn@gmail.com>
2019-12-11 13:08:41 +0100

    Añadido codigo

--- app/src/main/java/com/example/conexionbluetooth/EnablingBluetooth.java
+++ app/src/main/java/com/example/conexionbluetooth/EnablingBluetooth.java
@@ -1,9 +1,13 @@
 package com.example.conexionbluetooth;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,6 +19,35 @@ public class EnablingBluetooth extends AppCompatActivity {
 
     BluetoothAdapter mBluetoothAdapter;
 
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    private final BroadcastReceiver mBroadcastReceiver1 = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, mBluetoothAdapter.ERROR);
+
+                switch (state) {
+                    case BluetoothAdapter.STATE_OFF:
+                        Log.d(TAG,"onReceive: STATE OFF");
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_OFF:
+                        Log.d(TAG,"mBroadcastReceiver1: STATE OFF");
+                        break;
+                    case BluetoothAdapter.STATE_ON:
+                        Log.d(TAG,"mBroadcastReceiver1: STATE TURNING OFF");
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_ON:
+                        Log.d(TAG,"mBroadcastReceiver1: STATE TURNING ON");
+                        break;
+                }
+            }
+        }
+    };
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy: called.");
+        super.onDestroy();
+        unregisterReceiver(mBroadcastReceiver1);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,22 +57,32 @@ public class EnablingBluetooth extends AppCompatActivity {
         //Inicializo el adaptador
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
-        btnONOFF.setOnClickListener(new View.setOnClickListener() {
+        btnONOFF.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
+                Log.d(TAG, "onClick: enabling/disabling bluetooth.");
                 enableDisableBT();
+
             }
         });
     }
 
     public void enableDisableBT() {
         if (mBluetoothAdapter == null) {
-            log.d(TAG, "enableDisableBT: Does bot have BT capabilities.");
+            Log.d(TAG, "enableDisableBT: Does bot have BT capabilities.");
         }
-        if(!mBluetoothAdapter.isEnabled()) {
+        if (!mBluetoothAdapter.isEnabled()) {
+            Log.d(TAG, "enableDisableBT: enabling BT.");
             Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivity(enableBTTIntent);
+            startActivity(enableBTIntent);
 
             IntentFilter BTTIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            registerReceiver(mBroadcastReceiver1, BTTIntent);
+        }
+        if (mBluetoothAdapter.isEnabled()) {
+            Log.d(TAG, "enableDisableBT: disabling BT.");
+            mBluetoothAdapter.disable();
+            IntentFilter BTTIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            registerReceiver(mBroadcastReceiver1, BTTIntent);
         }
 
     }
